a708aa5923bdbb252e152636a4550fdde4b35279
ParthGaba03 <impgaba@gmail.com>
2025-08-15 00:52:54 +0530

    Prepare backend for deployment

--- marketplace_backend/settings.py
+++ marketplace_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@)a=@@k5of&#=^bex(8r$7e4i-0*k3s^x3aje##muidk1$fv#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
